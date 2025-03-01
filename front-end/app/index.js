4c3293c73a8150ac55564690d97695998a1d2c7e
Ayman Rahman <ayman2002rahman@gmail.com>
2025-03-01 01:02:20 -0600

    Setup tailwind

--- front-end/app/index.js
+++ front-end/app/index.js
@@ -1,12 +1,14 @@
 import { View, Text, Button } from "react-native";
 import { useRouter } from "expo-router";
+import tw from "../tailwind";
 
 export default function HomeScreen() {
   const router = useRouter();
   
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home Screen</Text>
+      <Text>Gym Pet</Text>
+      <Text style={tw`text-green-500`}>Here is the description of our app</Text>
       <Button title="Go to Profile" onPress={() => router.push("/profile")} />
     </View>
   );
---
+++ front-end/tailwind.js
@@ -0,0 +1,6 @@
+import twrnc from "twrnc";
+
+// Create a tw function for using Tailwind classes in React Native
+const tw = twrnc;
+
+export default tw;
