033d4c572a0ff3d4ca1e1b97493f667145ce4a95
pmahesh29 <pmahesh@uark.edu>
2025-03-01 14:50:04 -0600

    Add video playback to PetScreen and update dependencies

--- front-end/app/(auth)/pet.js
+++ front-end/app/(auth)/pet.js
@@ -1,16 +1,29 @@
-import { View, Text } from 'react-native';
-import tw from '../../tailwind';
+import { View } from 'react-native';
+import { Video } from 'expo-av';
 import { FAB } from 'react-native-paper';
 import { FontAwesome } from '@expo/vector-icons';
-import { router } from 'expo-router';
-
+import { useRouter } from 'expo-router';
+import tw from '../../tailwind';
 
 export default function PetScreen() {
+  const router = useRouter();
+
   return (
-    <View style={tw`flex-1 justify-center items-center`}>
-      <Text style={tw`text-xl font-bold`}>Pet Screen</Text>
+    <View style={tw`flex-1 justify-center items-center bg-white p-6`}>
+      <View pointerEvents="none">
+        <Video
+          source={require('../../assets/big-cat.mp4')}
+          rate={1.0}
+          volume={0.0} 
+          isMuted={true}
+          resizeMode="cover"
+          shouldPlay
+          isLooping
+          style={tw`w-72 h-72 rounded-lg`}
+        />
+      </View>
       <FAB
-        style={tw`absolute bottom-10 right-10`}
+        style={tw`absolute bottom-10 right-10 bg-green-500`}
         icon={() => <FontAwesome name="heartbeat" size={24} color="white" />}
         onPress={() => router.push('../workout/workoutSession')}
       />
