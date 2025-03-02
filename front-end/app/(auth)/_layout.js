5e27313d70663dd1be178c4cc436a6175234f367
Pranav Mahesh <102569626+pmahesh29@users.noreply.github.com>
2025-03-01 22:53:16 -0600

    Update _layout.js

--- front-end/app/(auth)/_layout.js
+++ front-end/app/(auth)/_layout.js
@@ -21,8 +21,52 @@ export default function AppLayout() {
         source={require('../../assets/petBackground.png')}
         style={tw`flex-1`}
       >
-        <SafeAreaView style={tw`flex-1`}>
+        {/* add the dumbell.png */}
+        <SafeAreaView style={tw`flex-1 bg-gray-900`}>
           <Tabs
+            screenOptions={{
+              tabBarActiveTintColor: '#4F46E5',
+              tabBarInactiveTintColor: 'gray',
+              tabBarStyle: {
+                backgroundColor: '#1a202c',
+                height: 60,
+                paddingBottom: 10,
+              },
+              headerStyle: {
+                backgroundColor: '#1a202c',
+              },
+              headerTintColor: '#fff',
+            }}>
+            <Tabs.Screen
+              name="workoutLogs"
+              options={{
+                title: '',
+                tabBarIcon: ({ color }) => (
+                  <FontAwesome name="clipboard" size={28} color={color} />
+                ),
+              }}
+            />
+            <Tabs.Screen
+              name="pet"
+              options={{
+                title: '',
+                tabBarIcon: ({ color }) => (
+                  <FontAwesome name="paw" size={28} color={color} />
+                ),
+              }}
+            />
+            <Tabs.Screen
+              name="profile"
+              options={{
+                title: '',
+                tabBarIcon: ({ color }) => (
+                  <FontAwesome name="user" size={24} color={color} />
+                ),
+              }}
+            />
+          </Tabs>
+        </SafeAreaView>
+          {/* <Tabs
             screenOptions={{
               tabBarActiveTintColor: '#4F46E5',
               tabBarInactiveTintColor: 'gray',
@@ -67,8 +111,7 @@ export default function AppLayout() {
               }}
             />
             
-          </Tabs>
-        </SafeAreaView>
+          </Tabs> */}
       </ImageBackground>
     </AuthProvider>
   );
